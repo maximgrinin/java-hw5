4e75c620be2281fd87d2d89a035f2ad99f82840c
maximgrinin <maximgrinin@gmail.com>
2022-11-14 10:12:43 +0300

    Code was formatted

--- src/Main.java
+++ src/Main.java
@@ -7,16 +7,17 @@ public class Main {
         // Level1:
         // Считывается 2 числа, произвести все арифметические операции с ними
         // (поделить, умножить, сложить, вычесть)
-        System.out.println();
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите число 1: ");
         int inputValue1 = scanner.nextInt();
         System.out.print("Введите число 2: ");
         int inputValue2 = scanner.nextInt();
+
         int sum = inputValue1 + inputValue2;
         int diff = inputValue1 - inputValue2;
         int mult = inputValue1 * inputValue2;
         double div = (double) inputValue1 / inputValue2;
+
         System.out.printf("%,d + %,d = %,d\n", inputValue1, inputValue2, sum);
         System.out.printf("%,d - %,d = %,d\n", inputValue1, inputValue2, diff);
         System.out.printf("%,d * %,d = %,d\n", inputValue1, inputValue2, mult);
@@ -29,12 +30,14 @@ public class Main {
         System.out.println();
         System.out.print("Введите число: ");
         int inputValue  = scanner.nextInt();
+
         int number = inputValue;
         int digit3 = number % 10;
         number /= 10;
         int digit2 = number % 10;
         number /= 10;
         int digit1 = number % 10;
+
         System.out.printf("Первая цифра: %d\nВторая цифра: %d\nТретья цифра: %d\n", digit1, digit2, digit3);
 
     }
